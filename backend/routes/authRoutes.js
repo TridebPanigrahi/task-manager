e21d8848edf36990c776cb5ccc0b2514bf08f9f1
Trideb <panigrahitrideb1@gmail.com>
2025-11-29 09:41:50 +0530

    Added October Changes to main

---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const {
+  registerUser,
+  loginUser,
+  getUserProfile,
+  updateUserProfile,
+} = require("../controllers/authControllers");
+const { protect } = require("../middlewares/authMiddleware");
+const upload = require("../middlewares/updateMiddleware");
+
+const router = express.Router();
+
+//Auth Routes
+
+router.post("/register", registerUser); // Register User
+router.post("/login", loginUser); // Login User
+router.get("/profile", protect, getUserProfile); // get User Profile
+router.put("/profile", protect, updateUserProfile); // update Profile
+
+router.post("/upload-image", upload.single("image"), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ message: "No file uploaded" });
+  }
+  const imageUrl = `${req.protocol}://${req.get("host")}/uploads/${
+    req.file.filename
+  }`;
+  res.status(200).json({ imageUrl });
+});
+
+module.exports = router;
